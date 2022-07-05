341b7c2730b1227c61a8375d8b7b9296005673af
Ibrah44 <ibrah.sseru@gmail.com>
2022-07-04 22:59:46 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main - main function .
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char h[] = "Ibrah";
+	int s = sizeof(h);
+	int i;
+
+	for (i = 0; i < s; i++)
+	{
+		_putchar(h[i]);
+
+	}
+
+	_putchar('\n');
+	return (0);
+}
+
