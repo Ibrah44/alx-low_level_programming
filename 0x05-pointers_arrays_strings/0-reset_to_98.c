5fe9a51f7c60fa910cdf0259334761a11175c777
Ibrah44 <ibrah.sseru@gmail.com>
2022-07-11 00:02:17 -0700

    task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,16 +1,17 @@
-#include "main.h"
+nclude "main.h"
+
+
 
 /**
- *
- *  * reset_to_98 - main
- *
- *   * @n: - pinter to int
- *
- *    */
+  * reset_to_98 - check the code for Holberton School students.
+   * @n : n is the pointer of n and return value 98
+    * Return: Always 0.
+      */
+
 void reset_to_98(int *n)
 
 {
 
-	*n = 98;
+		*n = 98;
 
 }
