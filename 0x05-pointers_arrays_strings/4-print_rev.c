c5c4a0def494f24148e6b9ff72b5c5b7371b0878
Ibrah44 <ibrah.sseru@gmail.com>
2022-07-11 00:51:47 -0700

    Task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,18 +2,21 @@
 /**
  * print_rev - Prints a string in reverse.
  * @s: string to be reserved.
+ * Return: always 0.
  */
 void print_rev(char *s)
 {
-	int len = 0, i;
+	int i = 0;
 
-	while (s[i++])
-		len++;
-
-	for (i = len - 1; i >= 0; i--)
+while (s[i] != '\0')
+	{
+		i++;
+	}
+	i--;
+	for (; i >= 0; i--)
+	{
 		_putchar(s[i]);
 
 	_putchar('\n');
 }
-
 
